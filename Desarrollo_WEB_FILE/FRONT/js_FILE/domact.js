de91bcfba2ef6ecc1c8a16e976de1e8a11a943e7
Fernanda Osorio <A01026502@tec.mx>
2023-03-28 11:43:43 -0600

    started dom act

---
+++ Desarrollo_WEB_FILE/FRONT/js_FILE/domact.js
@@ -0,0 +1,9 @@
+//q: what is dom?
+//a: dom is a document object model
+//q: what is the document object model?
+//a: the document object model is a programming interface for html and xml documents
+//q: que es el DOM?
+//a: el DOM es un modelo de objetos de documento
+//q: que es el modelo de objetos de documento?
+//a: el modelo de objetos de documento es una interfaz de programación para documentos HTML y XML
+
