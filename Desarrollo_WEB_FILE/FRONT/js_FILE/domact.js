f97ba49c5ceafb273d4b987310784e5413455b66
Fernanda Osorio <A01026502@tec.mx>
2023-03-29 13:11:32 -0600

    finished questions from dom

--- Desarrollo_WEB_FILE/FRONT/js_FILE/domact.js
+++ Desarrollo_WEB_FILE/FRONT/js_FILE/domact.js
@@ -1,9 +1,8 @@
-//q: what is dom?
-//a: dom is a document object model
-//q: what is the document object model?
-//a: the document object model is a programming interface for html and xml documents
-//q: que es el DOM?
-//a: el DOM es un modelo de objetos de documento
-//q: que es el modelo de objetos de documento?
-//a: el modelo de objetos de documento es una interfaz de programación para documentos HTML y XML
+
+function showMousePosition(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coor = "Posición del mouse: (" + x + "," + y + ")";
+    document.getElementById("mousePosition").innerHTML = coor;
+}
 
